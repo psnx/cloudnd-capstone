20d8dfc58d72b68e5c09317b8ebccb71fe01b120
Tamas Panyi <panyitamas@gmail.com>
2020-04-07 17:59:56 +0200

    Frontend update

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
-          :src="require('../assets/logo.svg')"
+          :src="require('../assets/exit.png')"
           class="my-3"
           contain
           height="200"
@@ -12,16 +12,12 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Cloud DevOps Engineer Nanodegree Program
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          Capstone Project Subussion
+          <br>Tamas Panyi
         </p>
       </v-col>
 
@@ -30,7 +26,7 @@
         cols="12"
       >
         <h2 class="headline font-weight-bold mb-3">
-          What's next?
+          Attemtpt 1
         </h2>
 
         <v-row justify="center">
